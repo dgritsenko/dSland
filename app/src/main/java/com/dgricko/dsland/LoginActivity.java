c2e337d4e537a4452aebaee02423e53bd0baebfc
dmytrohrytsenko <gritsenko.dhr@gmail.com>
2020-07-24 17:50:47 +0300

    admin can login to AdminActivity

--- app/src/main/java/com/dgricko/dsland/LoginActivity.java
+++ app/src/main/java/com/dgricko/dsland/LoginActivity.java
@@ -10,6 +10,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dgricko.dsland.model.User;
@@ -28,6 +29,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText inputPhone, inputPassword;
     private Button loginButton;
     private ProgressDialog loadingBar;
+    private TextView adminLink, userLink;
+
     private String parentDbName = "Users";
     private CheckBox rememberMeChk;
 
@@ -36,19 +39,41 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        inputPhone = (EditText)findViewById(R.id.login_phone_number_input);
-        inputPassword = (EditText)findViewById(R.id.login_password_input);
-        loginButton = (Button)findViewById(R.id.login_btn);
+        inputPhone = (EditText) findViewById(R.id.login_phone_number_input);
+        inputPassword = (EditText) findViewById(R.id.login_password_input);
+        adminLink = (TextView) findViewById(R.id.admin_panel_link);
+        userLink = (TextView) findViewById(R.id.user_panel_link);
+        loginButton = (Button) findViewById(R.id.login_btn);
         loadingBar = new ProgressDialog(this);
 
 
-        rememberMeChk = (CheckBox)findViewById(R.id.remember_checkbox);
+        rememberMeChk = (CheckBox) findViewById(R.id.remember_checkbox);
         Paper.init(this);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loginUser();
+            }
+        });
+
+        adminLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginButton.setText("Login Admin");
+                adminLink.setVisibility(View.INVISIBLE);
+                userLink.setVisibility(View.VISIBLE);
+                parentDbName = "Admins";
+            }
+        });
+
+        userLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginButton.setText("Login User");
+                adminLink.setVisibility(View.VISIBLE);
+                userLink.setVisibility(View.INVISIBLE);
+                parentDbName = "Users";
             }
         });
     }
@@ -58,25 +83,25 @@ public class LoginActivity extends AppCompatActivity {
         String phone = inputPhone.getText().toString();
         String password = inputPassword.getText().toString();
 
-        if (TextUtils.isEmpty(phone)){
+        if (TextUtils.isEmpty(phone)) {
             inputPhone.setBackgroundResource(R.drawable.error_design);
-        }else if (TextUtils.isEmpty(password)){
+        } else if (TextUtils.isEmpty(password)) {
             inputPassword.setBackgroundResource(R.drawable.error_design);
-        }else{
+        } else {
             loadingBar.setTitle("Login Account");
             loadingBar.setMessage("Please wait ,while we are checking you data");
             loadingBar.setCanceledOnTouchOutside(false);
             loadingBar.show();
 
-            allowAccessToAccount(phone,password);
+            allowAccessToAccount(phone, password);
         }
     }
 
     private void allowAccessToAccount(final String phone, final String password) {
 
-        if (rememberMeChk.isChecked()){
-            Paper.book().write(Prevalent.UserPhoneKey,phone);
-            Paper.book().write(Prevalent.UserPasswordKey,password);
+        if (rememberMeChk.isChecked()) {
+            Paper.book().write(Prevalent.UserPhoneKey, phone);
+            Paper.book().write(Prevalent.UserPasswordKey, password);
         }
 
         final DatabaseReference rootRef;
@@ -85,24 +110,33 @@ public class LoginActivity extends AppCompatActivity {
         rootRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.child(parentDbName).child(phone).exists()){
+                if (snapshot.child(parentDbName).child(phone).exists()) {
 
                     User userData = snapshot.child(parentDbName).child(phone).getValue(User.class);
 
-                    if (userData.getPhone().equals(phone)){
+                    if (userData.getPhone().equals(phone)) {
                         if (userData.getPassword().equals(password)) {
 
-                            Toast.makeText(LoginActivity.this, "logged in Successfully", Toast.LENGTH_SHORT).show();
-                            loadingBar.dismiss();
-                            Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
-                            startActivity(intent);
-                        }else{
+                            if (parentDbName.equals("Admins")) {
+                                Toast.makeText(LoginActivity.this, "Admin panel", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                                Intent intent = new Intent(LoginActivity.this, AdminActivity.class);
+                                startActivity(intent);
+                            } else if (parentDbName.equals("Users")) {
+
+                                Toast.makeText(LoginActivity.this, "logged in Successfully", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                            }
+
+                        } else {
                             loadingBar.dismiss();
                             Toast.makeText(LoginActivity.this, "Password in incorrect.", Toast.LENGTH_SHORT).show();
                         }
                     }
-                }else{
-                    Toast.makeText(LoginActivity.this, "Account with this "+phone+"number don't exist", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Account with this " + phone + "number don't exist", Toast.LENGTH_SHORT).show();
                     loadingBar.dismiss();
                 }
             }
